129c443fd8883da8dac231998b59ee7bb307831a
Brian Richter <brianorwhatever@gmail.com>
2024-04-11 23:22:01 -0700

    Using proper cryptosuite
    Signed-off-by: Brian Richter <brianorwhatever@gmail.com>

--- src/utils.ts
+++ src/utils.ts
@@ -1,1 +1,3 @@
 export const clone = (input: any) => JSON.parse(JSON.stringify(input));
+
+export const createDate = (created?: Date) => new Date(created ?? Date.now()).toISOString().slice(0,-5)+'Z'
